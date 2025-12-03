e1844ace5c0eafd42dd6a7b549cd77df9546f361
jkhanjan <jhakhanjan62@gmail.com>
2025-12-03 21:09:51 +0530

    context for user profile added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import './App.css'
-import AppRoutes from './routes/AppRoutes'
+import React from "react";
+import "./App.css";
+import AppRoutes from "./routes/AppRoutes";
+import { AuthProvider } from "./context/AuthContext";
 
 const App = () => {
   return (
-    <>
+    <AuthProvider>
       <AppRoutes />
-    </>
-  )
-}
+    </AuthProvider>
+  );
+};
 
-export default App
+export default App;
